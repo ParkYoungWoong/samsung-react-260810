c48a06b029fbf72cecc39f481344bf33b79783fa
ParkYoungWoong <thesecon@gmail.com>
2026-08-11 11:37:47 +0900

    260811-11:37

---
+++ api/movie.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+interface RequestBody {
+  title?: string
+  id?: string
+}
+
+export async function POST(request: Request) {
+  const { title, id } = (await request.json()) as RequestBody
+  const url = id
+    ? `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&i=${id}`
+    : `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${title}`
+  const { data } = await axios.get(url)
+  return Response.json(data)
+}
